170d659b0c4fca618ab1069bfc3e86b83f413b91
Igor Magalhaes <igor.magalhaes.r@gmail.com>
2024-04-16 19:27:35 -0300

    active search working

--- src/app/client/positions.py
+++ src/app/client/positions.py
@@ -5,6 +5,7 @@ from fastcrud import FastCRUD
 from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, func
 
 from .config import templates
 from ..models.position import Position
@@ -39,3 +40,36 @@ async def get_positions(request: Request, db: Annotated[AsyncSession, Depends(as
                 "rows_per_page": limit
             },
         )
+
+
+@router.get("/search_positions", response_class=HTMLResponse)
+async def search_positions(request: Request, db: Annotated[AsyncSession, Depends(async_get_db)], search: str = "", page: int = 1):
+    limit = int(request.query_params.get("rows-per-page-select", 10))
+    offset = (page - 1) * limit
+
+    query = select(Position).offset(offset).limit(limit)
+    if search:
+        query = query.where(Position.title.ilike(f"%{search}%"))
+
+    positions_result = await db.execute(query)
+    positions = positions_result.scalars().all()
+
+    total_count_query = select(func.count()).select_from(Position)
+    if search:
+        total_count_query = total_count_query.where(Position.title.ilike(f"%{search}%"))  # Updated to use ilike
+    total_count_result = await db.execute(total_count_query)
+    total_count = total_count_result.scalar_one()
+
+    total_pages = ceil(total_count / limit)
+
+    return templates.TemplateResponse(
+        "positions_table.html", 
+        {
+            "request": request,
+            "positions": positions,
+            "total_items": total_count,
+            "current_page": page,
+            "total_pages": total_pages,
+            "rows_per_page": limit
+        }
+    )
--- src/app/client/postings.py
+++ src/app/client/postings.py
@@ -5,6 +5,7 @@ from fastcrud import FastCRUD
 from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, func
 
 from .config import templates
 from ..models.posting import Posting
@@ -20,9 +21,7 @@ async def get_postings(request: Request, db: Annotated[AsyncSession, Depends(asy
     offset = (page - 1) * limit
 
     postings = await crud.get_multi(db=db, offset=offset, limit=limit)
-    
     total_pages = ceil(postings["total_count"] / limit)
-
 
     template = "postings.html"
     if "HX-Request" in request.headers:
@@ -39,3 +38,36 @@ async def get_postings(request: Request, db: Annotated[AsyncSession, Depends(asy
                 "rows_per_page": limit
             },
         )
+
+
+@router.get("/search_postings", response_class=HTMLResponse)
+async def search_postings(request: Request, db: Annotated[AsyncSession, Depends(async_get_db)], search: str = "", page: int = 1):
+    limit = int(request.query_params.get("rows-per-page-select", 10))
+    offset = (page - 1) * limit
+
+    query = select(Posting).offset(offset).limit(limit)
+    if search:
+        query = query.where(Posting.original_title.ilike(f"%{search}%"))
+
+    postings_result = await db.execute(query)
+    postings = postings_result.scalars().all()
+
+    total_count_query = select(func.count()).select_from(Posting)
+    if search:
+        total_count_query = total_count_query.where(Posting.original_title.ilike(f"%{search}%"))
+    total_count_result = await db.execute(total_count_query)
+    total_count = total_count_result.scalar_one()
+
+    total_pages = ceil(total_count / limit)
+
+    return templates.TemplateResponse(
+        "postings_table.html", 
+        {
+            "request": request,
+            "postings": postings,
+            "total_items": total_count,
+            "current_page": page,
+            "total_pages": total_pages,
+            "rows_per_page": limit
+        }
+    )
