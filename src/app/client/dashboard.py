f12fa8f2209cadbef8ddf55c84ca5be6ba267ed1
Igor Magalhaes <igor.magalhaes.r@gmail.com>
2024-04-17 02:03:00 -0300

    first version of dashboard working

--- src/app/client/dashboard.py
+++ src/app/client/dashboard.py
@@ -1,19 +1,19 @@
 from typing import Annotated
+import statistics
 
-from fastcrud import FastCRUD
-from fastapi import APIRouter, Request, Depends
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi import APIRouter, Request, Depends, HTTPException
+from fastapi.responses import HTMLResponse
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, distinct
+from sqlalchemy import select, distinct, func
+
 
 from .config import templates
 from ..models.position import Position
 from ..models.posting import Posting
+from ..models.salary import Salary
 from ..core.db.database import async_get_db
 
 router = APIRouter(tags=["client_dashboard"])
-
-crud = FastCRUD(Position)
 
 @router.get("/dashboard", response_class=HTMLResponse)
 async def get_dashboard(request: Request):
@@ -42,3 +42,44 @@ async def get_dropdown(request: Request, db: Annotated[AsyncSession, Depends(asy
             document.getElementById('company-select').innerHTML = '<option value="">Select a company...</option>{companies_html}';
         </script>
     """)
+
+
+@router.get("/salary_distribution/")
+async def get_salary_distribution(
+    position: str,
+    db: AsyncSession = Depends(async_get_db)
+):
+    query = select(
+        Salary.base_salary.label('salary'),
+        func.count().label('count')
+    ).join(
+        Posting, Posting.id == Salary.posting_id
+    ).join(
+        Position, Position.id == Posting.position_id
+    ).where(
+        func.lower(Position.title).like(f"%{position.lower()}%")
+    ).group_by(
+        Salary.base_salary
+    ).order_by(
+        Salary.base_salary
+    )
+
+    result = await db.execute(query)
+    salaries = result.all()
+    if not salaries:
+        raise HTTPException(status_code=404, detail="No salary data found for the given position.")
+    
+    salary_list = [salary for salary, count in salaries for _ in range(count)]
+    mean_salary = statistics.mean(salary_list) if salary_list else 0
+    median_salary = statistics.median(salary_list) if salary_list else 0
+    std_deviation = statistics.stdev(salary_list) if len(salary_list) > 1 else 0
+
+    salary_distribution = {
+        'salaries': [{'salary': salary, 'count': count} for salary, count in salaries],
+        'statistics': {
+            'mean': mean_salary,
+            'median': median_salary,
+            'std_deviation': std_deviation
+        }
+    }
+    return salary_distribution
